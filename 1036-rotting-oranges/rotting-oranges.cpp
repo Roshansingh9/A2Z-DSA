6ff2431c65b1620474f3df8aacb0c99e69a58eee
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2026-01-20 13:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (97.04%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,38 +3,45 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int rotten=0,fresh=0,time=0;
-        queue<pair<pair<int,int>, int>> q;
+        queue<pair<pair<int, int>, int>> q;
+        int max_time = 0;
+        int vis[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
-                    q.push({{i, j},0});
-                
-                }else if(grid[i][j]==1){
-                    fresh++;
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 2;
+                } else {
+                    vis[i][j] = 1;
                 }
             }
         }
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
+        int dr[] = {0, -1, 0, 1};
+        int dc[] = {-1, 0, 1, 0};
         while (!q.empty()) {
+            int t = q.front().second;
             int r = q.front().first.first;
             int c = q.front().first.second;
-            int t = q.front().second;
-            time=max(time,t);
+            max_time = max(max_time, t);
             q.pop();
             for (int i = 0; i < 4; i++) {
-                int nr = r + dx[i];
-                int nc = c + dy[i];
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
-                    grid[nr][nc] == 1) {
-                    
-                    rotten++;
-                    grid[nr][nc] = 2;
-                    q.push({{nr, nc},t+1});
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (nr < n && nr >= 0 && nc < m && nc >= 0 &&
+                    vis[nr][nc] != 2 && grid[nr][nc] == 1) {
+                    q.push({{nr, nc}, t + 1});
+                    vis[nr][nc] = 2;
                 }
             }
         }
-        return rotten==fresh?time:-1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] != 2 && grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return max_time;
     }
 };
